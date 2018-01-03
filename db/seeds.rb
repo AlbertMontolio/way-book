f804f8c70811ece1d75fae0779f0277be4cb82b2
Albert Montolio <albertmontolio@Alberts-MacBook-Pro.local>
2018-01-03 13:47:46 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,52 @@
 # it, engineering, people plus, 
+skills = [
+	{
+		category: "SE-Team Leiter",
+		category_skills: ["Boole Tool", "CAD-PDM", "Catia v5", "Freigabeleitstand", "Freigaben", "gAMS", "GSS", "ip3 Terminplan", "ItO Workplace", "KMG/MMG", "LOP", "PEP PDM", "PRISMA", "Projektbedarf", "SKYPE", "TWEN", "TAIS", "PRISMA", "CARISMA", "PQM", "Catia v5"]
+	},
+	{
+		category: "Modul Leiter",
+		category_skills: ["AKV", "Baukasten", "Dali & DBV", "DBKR", "Einkaufswagen", "ItO", "Lastenheft", "Lieferant", "Modularbeit", "PMC", "Problemmanagement", "Qualitätsmgmt - Six Sigma - 5W/8D Methode", "Reifegradmanagement", "SBWE", "Virtuelles Gesamtfahrzeug", "Catia v5", "Prisma", "CAD-PDM", "M-gAMS", "Project Management"]
+	},
+	{
+		category: "IT-Support",
+		category_skills: ["Windows Clients 7/10", "Windows Server 2008", "Windows Server 2012", "Android", "iOS", "Microsoft Exchange", "Microsoft SQL", "Virtualisierung VMware", "Virtualisierung HyperV", "IPv4", "IPv6", "Firewalls", "DHCP", "DNS", "VLAN", "Internet-Domains", "DNS Verwaltung", "Email-Workflow", "Antispam"]
+	},
+	{
+		category: "Progammierer",
+		category_skills: ["Ruby", "Rails", "JavaScript", "HTML", "CSS", "Daten Banken", "Java", "Python", "R", "Visual Basic", "Android", "Swift", "iOS", "Linux", "Active Record", "MySql", "Node.js"]
+	},
+	{
+		category: "Project Management",
+		category_skills: ["Leadership", "Team Management", "RPlan", "Microsoft Office Project Management", "Konfliktmanagement", "PLM: Product-Lifecycle-Management"]
+	},
+	{
+		category: "Business Developer",
+		category_skills: ["Excel", "Market analyse", "Konkurrenz Analyse", "Visual Basic", "R", "Python", "PowerPoint", "Kunde Betreung", "Messe Erfahrung", "Workshops Vorbereitung"]
+	},
+	{
+		category: "Konstruktor",
+		category_skills: ["Catia v5", "Catia v6", "Automotive Erfahrung", "Bauteil Kenntnisse", "Zeichnungen", "Solid Works"]
+	},
+	{
+		category: "Languages",
+		category_skills: ["Deutsch", "Englisch", "Spanisch", "Polnisch", "Chinesisch"]
+	},
+	{
+		category: "Karosserie Module",
+		category_skills: ["KA: Bodengruppe", "KB: Rohbau, Seitengerippe, Dach", "KC: Dach-, Verdecksysteme, Einfüll-Ladeklappen", "KD: Frontsystem, Kunststoffexterieur, Stossfänger", "KE: Einstieg, Türsystem", "KF: Heckklappe, Front-, Heckscheibe, Reinigungssystem", "KG: Licht, Karosserieelektronik"]
+	},
+]
+
+skills.each do |skill_info|
+	category = Category.create(name: skill_info[:category])
+	skill_info[:category_skills].each do |skill|
+		skill = CompanySkill.new(name: skill)
+		skill.category = category
+		skill.save
+	end
+end
+
 
 employees = [
 	{
@@ -12,6 +60,16 @@ employees = [
 		position: "Mechanical Engineer & Web Developer",
 		title: "Lebenslauf Deutsch",
 		document: "image/upload/v1514324187/edzm3iz0vfzn3fte8vw6.pdf",
+		own_company_skills: [
+			{
+				category: "Progammierer",
+				names: ["Ruby", "Rails", "JavaScript", "HTML", "CSS", "Active Record"]
+			},
+			{
+				category: "SE-Team Leiter",
+				names: ["Boole Tool", "CAD-PDM", "Catia v5", "Freigabeleitstand"],
+			}
+		],
 		projects: [
 			{
 				title: "SE-Team Leiter i15 Türen",
@@ -67,152 +125,17 @@ employees = [
 		email: "alexander.kir@waygroup.de",
 		password: "alexander.kir@waygroup.de",
 		division: "Engineering",
-		team: "Christian Sailer",
+		team: "TBD",
 		first_name: "Alexander",
 		last_name: "Kir",
-		phone_number: "+491546430434",
-		position: "SE-Team Leiter",
-		title: "Lebenslauf English",
-		document: "image/upload/v1514369271/linda_rinaldi_ez8iyl.pdf",
-		projects: [
-			{
-				title: "SE-Team Leiter",
-				client: "Amazon",
-				start: Date.new(2016,1,2),
-				finish: Date.new(2017,12,20),
-			},
-			{
-				title: "Carbures Carbon Fiber Projekte",
-				client: "Media Markt",
-				start: Date.new(2015,4,12),
-				finish: Date.new(2016,1,2),
-			},
-			{
-				title: "Entwicklungsingenieur Karrosserie Lkvs",
-				client: "MAN AG",
-				start: Date.new(2014,3,22),
-				finish: Date.new(2015,4,12),
-			},
-			{
-				title: "Berechnungssingenieur",
-				client: "BMW AG",
-				start: Date.new(2011,1,1),
-				finish: Date.new(2014,3,22),
-			}
+		phone_number: "TBD",
+		position: "TBD",
+		title: "TBD",
+		own_company_skills: [
+			
 		],
-	},
-	{
-		email: "andre.kolks@waygroup.de",
-		password: "andre.kolks@waygroup.de",
-		division: "Engineering",
-		team: "Christian Sailer",
-		first_name: "Andre",
-		last_name: "Kolks",
-		phone_number: "+491546858585",
-		position: "Modul Leiter",
-		title: "Lebenslauf Deutsch",
-		document: "image/upload/v1514369271/linda_rinaldi_ez8iyl.pdf",
 		projects: [
-			{
-				title: "Modul Leiter KD",
-				client: "BMW Group",
-				start: Date.new(2016,1,2),
-				finish: Date.new(2017,12,20),
-			},
-			{
-				title: "Modul Leiter KE",
-				client: "BMW Group",
-				start: Date.new(2015,4,12),
-				finish: Date.new(2016,1,2),
-			},
-			{
-				title: "Entwicklungsingenieur Karrosserie Lkvs",
-				client: "MAN AG",
-				start: Date.new(2014,3,22),
-				finish: Date.new(2015,4,12),
-			},
-			{
-				title: "Berechnungssingenieur",
-				client: "BMW AG",
-				start: Date.new(2011,1,1),
-				finish: Date.new(2014,3,22),
-			}
-		],
-	},
-	{
-		email: "beyrem.amor@waygroup.de",
-		password: "beyrem.amor@waygroup.de",
-		division: "Engineering",
-		team: "Christian Sailer",
-		first_name: "Beyrem",
-		last_name: "Amor",
-		phone_number: "+491324298756",
-		position: "SE-Team Leiter",
-		title: "Lebenslauf Deutsch",
-		document: "image/upload/v1514369271/linda_rinaldi_ez8iyl.pdf",
-		projects: [
-			{
-				title: "SE-Team Leiter i15 Türen",
-				client: "BMW Grouop",
-				start: Date.new(2016,1,2),
-				finish: Date.new(2017,12,20),
-			},
-			{
-				title: "Business Developer Carbon Fiber parts",
-				client: "Carbures Group",
-				start: Date.new(2015,4,12),
-				finish: Date.new(2016,1,2),
-			},
-			{
-				title: "Entwicklungsingenieur Karrosserie Lkvs",
-				client: "MAN AG",
-				start: Date.new(2014,3,22),
-				finish: Date.new(2015,4,12),
-			},
-			{
-				title: "Berechnungssingenieur",
-				client: "BMW AG",
-				start: Date.new(2011,1,1),
-				finish: Date.new(2014,3,22),
-			}
-		],
-	},
-	{
-		email: "florian.sassl@waygroup.de",
-		password: "florian.sassl@waygroup.de",
-		division: "Engineering",
-		team: "Brigitte Schulz",
-		first_name: "Florian",
-		last_name: "Sassl",
-		phone_number: "+491526792656",
-		position: "SE-Team Leiter",
-		title: "Lebenslauf English",
-		document: "image/upload/v1514369271/linda_rinaldi_ez8iyl.pdf",
-		projects: [
-			{
-				title: "SE-Team Leiter i15 Türen",
-				client: "BMW Grouop",
-				start: Date.new(2016,1,2),
-				finish: Date.new(2017,12,20),
-			},
-			{
-				title: "Business Developer Carbon Fiber parts",
-				client: "Carbures Group",
-				start: Date.new(2015,4,12),
-				finish: Date.new(2016,1,2),
-			},
-			{
-				title: "Entwicklungsingenieur Karrosserie Lkvs",
-				client: "MAN AG",
-				start: Date.new(2014,3,22),
-				finish: Date.new(2015,4,12),
-			},
-			{
-				title: "Berechnungssingenieur",
-				client: "BMW AG",
-				start: Date.new(2011,1,1),
-				finish: Date.new(2014,3,22),
-			}
+			
 		],
 	},
 	{
@@ -222,74 +145,175 @@ employees = [
 		team: "Brigitte Schulz",
 		first_name: "Alois",
 		last_name: "Stein",
-		phone_number: "+491523333656",
+		phone_number: "TBD",
 		position: "Recruiter",
-		title: "Lebenslauf English",
-		document: "image/upload/v1514369271/linda_rinaldi_ez8iyl.pdf",
-		projects: [
+		title: "TBD",
+		own_company_skills: [
 			{
-				title: "Recruiting",
-				client: "WAY Group",
-				start: Date.new(2016,1,2),
-				finish: Date.new(2017,12,20),
+				category: "Karosserie Module",
+				names: ["KD: Frontsystem, Kunststoffexterieur, Stossfänger", "KF: Heckklappe, Front-, Heckscheibe, Reinigungssystem", "KA: Bodengruppe", "KB: Rohbau, Seitengerippe, Dach", "KC: Dach-, Verdecksysteme, Einfüll-Ladeklappen"]
 			},
 			{
-				title: "SE-Team Leiter",
-				client: "BMW Group",
-				start: Date.new(2015,4,12),
-				finish: Date.new(2016,1,2),
-			},
-			{
-				title: "Entwicklungsingenieur Karrosserie Lkvs",
-				client: "MAN AG",
-				start: Date.new(2014,3,22),
-				finish: Date.new(2015,4,12),
-			},
-			{
-				title: "Berechnungssingenieur",
-				client: "BMW AG",
-				start: Date.new(2011,1,1),
-				finish: Date.new(2014,3,22),
+				category: "SE-Team Leiter",
+				names: ["gAMS", "PQM"],
 			}
+		],
+		projects: [
+			
 		],
 	},
 	{
-		email: "veronika.hollerith@waygroup.de",
-		password: "veronika.hollerith@waygroup.de",
+		email: "alvaro.leal@waygroup.de",
+		password: "alvaro.leal@waygroup.de",
 		division: "Engineering",
 		team: "Brigitte Schulz",
-		first_name: "Veronika",
-		last_name: "Hollerith",
-		phone_number: "+4915239876",
-		position: "SE-Team Leiter",
-		title: "Lebenslauf English",
+		first_name: "Alvaro",
+		last_name: "Leal",
+		phone_number: "TBD",
+		position: "TBD",
+		title: "TBD",
 		document: "image/upload/v1514369271/linda_rinaldi_ez8iyl.pdf",
-		projects: [
+		own_company_skills: [
 			{
-				title: "SE-Team Leiter i15 Türen",
-				client: "BMW Grouop",
-				start: Date.new(2016,1,2),
-				finish: Date.new(2017,12,20),
+				category: "Project Management",
+				names: ["PLM: Product-Lifecycle-Management"]
 			},
 			{
-				title: "Business Developer Carbon Fiber parts",
-				client: "Carbures Group",
-				start: Date.new(2015,4,12),
-				finish: Date.new(2016,1,2),
-			},
-			{
-				title: "Entwicklungsingenieur Karrosserie Lkvs",
-				client: "MAN AG",
-				start: Date.new(2014,3,22),
-				finish: Date.new(2015,4,12),
-			},
-			{
-				title: "Berechnungssingenieur",
-				client: "BMW AG",
-				start: Date.new(2011,1,1),
-				finish: Date.new(2014,3,22),
+				category: "Konstruktor",
+				names: ["Catia v5", "Catia v6"],
 			}
 		],
+		projects: [],
+	},
+	{
+		email: "anas.mnif@waygroup.de",
+		password: "anas.mnif@waygroup.de",
+		division: "TBD",
+		team: "TBD",
+		first_name: "Anas",
+		last_name: "Mnif",
+		phone_number: "TBD",
+		position: "TBD",
+		title: "TBD",
+		own_company_skills: [],
+		projects: [],
+	},
+	{
+		email: "andre.kolks@waygroup.de",
+		password: "andre.kolks@waygroup.de",
+		division: "Engineering",
+		team: "Christian Sailer",
+		first_name: "Andre",
+		last_name: "Kolks",
+		phone_number: "TBD",
+		position: "Modul Leiter",
+		title: "TBD",
+		own_company_skills: [],
+		projects: [],
+	},
+	{
+		email: "andreas.hauswald@waygroup.de",
+		password: "andreas.hauswald@waygroup.de",
+		division: "TBD",
+		team: "TBD",
+		first_name: "TBD",
+		last_name: "TBD",
+		phone_number: "TBD",
+		position: "TBD",
+		title: "TBD",
+		own_company_skills: [],
+		projects: [],
+	},
+	{
+		email: "anke.gradl@waygroup.de",
+		password: "anke.gradl@waygroup.de",
+		division: "TBD",
+		team: "TBD",
+		first_name: "TBD",
+		last_name: "TBD",
+		phone_number: "TBD",
+		position: "TBD",
+		title: "TBD",
+		own_company_skills: [],
+		projects: [],
+	},
+	{
+		email: "anton.sapronov@waygroup.de",
+		password: "anton.sapronov@waygroup.de",
+		division: "TBD",
+		team: "TBD",
+		first_name: "TBD",
+		last_name: "TBD",
+		phone_number: "TBD",
+		position: "TBD",
+		title: "TBD",
+		own_company_skills: [],
+		projects: [],
+	},
+	{
+		email: "beyrem.amor@waygroup.de",
+		password: "beyrem.amor@waygroup.de",
+		division: "TBD",
+		team: "TBD",
+		first_name: "TBD",
+		last_name: "TBD",
+		phone_number: "TBD",
+		position: "TBD",
+		title: "TBD",
+		own_company_skills: [],
+		projects: [],
+	},
+	{
+		email: "brigitte.schulz@waygroup.de",
+		password: "brigitte.schulz@waygroup.de",
+		division: "Engineering",
+		team: "Brigitte Schulz",
+		first_name: "Brigitte",
+		last_name: "Schulz",
+		phone_number: "TBD",
+		position: "Prokuristin",
+		title: "TBD",
+		own_company_skills: [],
+		projects: [],
+	},
+	{
+		email: "carolin.mumm@waygroup.de",
+		password: "carolin.mumm@waygroup.de",
+		division: "TBD",
+		team: "TBD",
+		first_name: "TBD",
+		last_name: "TBD",
+		phone_number: "TBD",
+		position: "TBD",
+		title: "TBD",
+		own_company_skills: [],
+		projects: [],
+	},
+	{
+		email: "christian.sailer@waygroup.de",
+		password: "christian.sailer@waygroup.de",
+		division: "TBD",
+		team: "TBD",
+		first_name: "TBD",
+		last_name: "TBD",
+		phone_number: "TBD",
+		position: "TBD",
+		title: "TBD",
+		own_company_skills: [],
+		projects: [],
+	},
+	{
+		email: "christoph.pincemin@waygroup.de",
+		password: "christoph.pincemin@waygroup.de",
+		division: "TBD",
+		team: "TBD",
+		first_name: "TBD",
+		last_name: "TBD",
+		phone_number: "TBD",
+		position: "TBD",
+		title: "TBD",
+		own_company_skills: [],
+		projects: [],
 	},
 ]
 
@@ -301,6 +325,17 @@ employees.each do |employee|
 	profile.user = user
 	profile.save
 
+	employee[:own_company_skills].each do |pair|
+		pair[:names].each do |name|
+			own_company_skill = OwnCompanySkill.new(name: name)
+			category = Category.where(name: pair[:category])[0]
+			own_company_skill.category = category
+			own_company_skill.profile = profile
+			own_company_skill.rating = rand(1..3)
+			own_company_skill.save
+		end
+	end
+
 	employee[:projects].each do |project|
 		project = Project.new(title: project[:title], client: project[:client], start: project[:start], finish: project[:finish])
 		project.profile = profile
@@ -310,94 +345,13 @@ end
 
 
 
-CATEGORIES = ["SE-Team Leiter", "Modul Leiter", "IT-Support", "Programmierer", "Project Management", "Business Developer", "Konstruktor", "Languages"]
-
-CATEGORIES.each do |name|
-	Category.create(name: name)
-end
-
-COMPANY_SKILLS_SE_TEAM_LEITER = ["Boole Tool", "CAD-PDM", "Catia v5", "Freigabeleitstand", "Freigaben", "gAMS", "GSS", "ip3 Terminplan", "ItO Workplace", "KMG/MMG", "LOP", "PEP PDM", "PRISMA", "Projektbedarf", "SKYPE", "TWEN", "TAIS", "PRISMA", "CARISMA", "PQM", "Catia v5"]
-
-seteamleiter = Category.where(name: "SE-Team Leiter")[0]
-
-COMPANY_SKILLS_SE_TEAM_LEITER.each do |name|
-	skill = CompanySkill.new(name: name)
-	skill.category = seteamleiter
-	skill.save
-end
 
 
-COMPANY_SKILLS_MODUL_LEITER = ["AKV", "Baukasten", "Dali & DBV", "DBKR", "Einkaufswagen", "ItO", "Lastenheft", "Lieferant", "Modularbeit", "PMC", "Problemmanagement", "Qualitätsmgmt - Six Sigma - 5W/8D Methode", "Reifegradmanagement", "SBWE", "Virtuelles Gesamtfahrzeug", "Catia v5", "Prisma", "CAD-PDM", "M-gAMS", "Project Management"]
-
-modulleiter = Category.where(name: "Modul Leiter")[0]
-
-COMPANY_SKILLS_MODUL_LEITER.each do |name|
-	skill = CompanySkill.new(name: name)
-	skill.category = modulleiter
-	skill.save
-end
-
-COMPANY_SKILLS_IT_SUPPORT = ["Windows Clients 7/10", "Windows Server 2008", "Windows Server 2012", "Android", "iOS", "Microsoft Exchange", "Microsoft SQL", "Virtualisierung VMware", "Virtualisierung HyperV", "IPv4", "IPv6", "Firewalls", "DHCP", "DNS", "VLAN", "Internet-Domains", "DNS Verwaltung", "Email-Workflow", "Antispam"]
-
-it_suport = Category.where(name: "IT-Support")[0]
-
-COMPANY_SKILLS_IT_SUPPORT.each do |name|
-	skill = CompanySkill.new(name: name)
-	skill.category = it_suport
-	skill.save
-end
-
-COMPANY_SKILLS_PROGRAMMIERER = ["Ruby", "Rails", "JavaScript", "HTML", "CSS", "Daten Banken", "Java", "Python", "R", "Visual Basic", "Android", "Swift", "iOS", "Linux", "Active Record", "MySql", "Node.js"]
-
-programmierer = Category.where(name: "Programmierer")[0]
-
-COMPANY_SKILLS_PROGRAMMIERER.each do |name|
-	skill = CompanySkill.new(name: name)
-	skill.category = programmierer
-	skill.save
-end
-
-COMPANY_SKILLS_PROJECT_MANAGEMENT = ["Leadership", "Team Management", "RPlan", "Microsoft Office Project Management", "Konfliktmanagement"]
-
-project_management = Category.where(name: "Project Management")[0]
-
-COMPANY_SKILLS_PROJECT_MANAGEMENT.each do |name|
-	skill = CompanySkill.new(name: name)
-	skill.category = project_management
-	skill.save
-end
 
 
-COMPANY_SKILLS_BUSINESS_DEVELOPER = ["Excel", "Market analyse", "Konkurrenz Analyse", "Visual Basic", "R", "Python", "PowerPoint", "Kunde Betreung", "Messe Erfahrung", "Workshops Vorbereitung"]
-
-business_developer = Category.where(name: "Business Developer")[0]
-
-COMPANY_SKILLS_BUSINESS_DEVELOPER.each do |name|
-	skill = CompanySkill.new(name: name)
-	skill.category = business_developer
-	skill.save
-end
 
 
-COMPANY_SKILLS_KONSTRUKTUR = ["CATIA", "Automotive Erfahrung", "Bauteil Kenntnisse", "Zeichnungen", "Solid Works"]
 
-konstruktor = Category.where(name: "Konstruktor")[0]
-
-COMPANY_SKILLS_KONSTRUKTUR.each do |name|
-	skill = CompanySkill.new(name: name)
-	skill.category = konstruktor
-	skill.save
-end
-
-COMPANY_SKILLS_LANGUAGES = ["Deutsch", "Englisch", "Spanisch", "Polnisch", "Chinesisch"]
-
-languages = Category.where(name: "Languages")[0]
-
-COMPANY_SKILLS_LANGUAGES.each do |name|
-	skill = CompanySkill.new(name: name)
-	skill.category = languages
-	skill.save
-end
 
 
 
