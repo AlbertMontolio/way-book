e421c947e5c5b096b0f5f604c81b8b67c61fedb7
Albert Montolio <albertmontolio@Alberts-MacBook-Pro.local>
2018-01-11 20:03:06 +0100

    done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,12 +5,6 @@ class PagesController < ApplicationController
   	puts "----1111111-------------------------------"
   	puts "current_user.email"
   	puts current_user.email
-  	puts "-------22222-----------------------------"
-  	puts "current_user.profile"
-  	puts current_user.profile
-  	puts "-------3333-----------------------------"
-  	puts "current_user.profile.first_name"
-  	puts current_user.profile.first_name
-  	puts "----------4444--------------------------"
+
   end
 end
