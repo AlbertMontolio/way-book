ede82a3fdb353e992109b904d437ade2be3354ac
Albert Montolio <albertmontolio@Alberts-MacBook-Pro.local>
2018-01-13 12:36:22 +0100

    done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,18 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-  	puts "----1111111-------------------------------"
-  	puts "current_user.email"
-  	# puts current_user.email
+  	puts "-----------------------------------"
+  	puts "PagesController home"
+  	puts "-----------------------------------"
+  	if user_signed_in?
+  		puts "current_user"
+  		puts current_user
 
+  		puts "current_user.profile"
+  		puts current_user.profile
+
+  		puts "current_user.profile.first_name"
+  		puts current_user.profile.first_name
+  	end
   end
 end
