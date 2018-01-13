91f0c098b99f6b88043751210fbebfc8139e25df
Albert Montolio <albertmontolio@Alberts-MacBook-Pro.local>
2018-01-13 12:42:40 +0100

    done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,11 +6,29 @@ class PagesController < ApplicationController
   	puts "PagesController home"
   	puts "-----------------------------------"
   	if user_signed_in?
-  		puts "current_user"
-  		puts current_user
+  		puts "current_user.id"
+  		puts current_user.id
+  		puts "---------------------------------"
+
+  		puts "current_user.email"
+  		puts current_user.email
+  		puts "---------------------------------"
+
+  		puts "current_user.password"
+  		puts current_user.password
+  		puts "----------------------------------"
 
   		puts "current_user.profile"
   		puts current_user.profile
+  		puts "-------------------------------------"
+
+  		puts "current_user.profile.id"
+  		puts current_user.profile.id
+  		puts "-------------------------------------"
+
+  		puts "current_user.profile.division"
+  		puts current_user.profile.division
+  		puts "-------------------------------------"
 
   		puts "current_user.profile.first_name"
   		puts current_user.profile.first_name
